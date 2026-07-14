4b872e081141af2cf16713b8c8457a59c5eaccf1
NGUYEN QUANG ANH <anhnqhe186704@fpt.edu.vn>
2026-07-15 06:05:21 +0700

    xác nhận hồ sơ và gửi thông báo cho bệnh nhân

--- src/main/java/com/example/mediscanauth/repository/PatientRepository.java
+++ src/main/java/com/example/mediscanauth/repository/PatientRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long>, JpaSpecificationExecutor<Patient> {
-
     Optional<Patient> findByUser(User user);
 
+    List<Patient> findAllByOrderByCreatedAtDesc();
 }
